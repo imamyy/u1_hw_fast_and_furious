45c09188ad7e4e07ff73d9ec4ba5f7245a242767
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-03 16:28:32 -0600

    Completed

--- script.js
+++ script.js
@@ -18,18 +18,42 @@ window.onload =  function(event) {
   randomQuote();
 
   // Do all of your work inside the window.onload function (in other words, here!)
-
+  
   // Part 1
-
+  document.getElementById('main-title').innerText = 'DOM Toretto';
   // Part 2
-
+  document.body.style.backgroundColor = '#85a651';
   // Part 3
-
+  const favThings = document.querySelectorAll('#favorite-things li')
+  favThings[favThings.length - 1].remove()
   // Part 4
+  const specTitle = document.querySelectorAll('.special-title')
+  specTitle.forEach(element => {
+    element.style.fontSize = '2rem'
+  }) 
 
   // Part 5
-
+  const pastRaces = document.querySelectorAll('#past-races li')
+  pastRaces.forEach((element) => {
+    if (element.innerText === 'Chicago'){
+      element.remove()
+    }
+  })
   // Part 6
-
+  const houston = document.createElement('li')
+  houston.innerText = 'Houston'
+  document.getElementById('past-races').appendChild(houston)
   // Part 7
+  const divHouston = document.createElement('div')
+  divHouston.classList.add('blog-post' , 'purple')
+  
+
+  const h1Houston = document.createElement('h1')
+  h1Houston.innerHTML = 'Houston'
+  const pHouston = document.createElement('p')
+  pHouston.innerText = "I JUST RACE FOR FUN"
+  divHouston.appendChild(h1Houston)
+  divHouston.appendChild(pHouston)
+  document.querySelector('.main').appendChild(divHouston)
+  
 }
